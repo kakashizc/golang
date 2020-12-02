0dbff79a1740f212ebd951f580f863addaf9a216
kakashizc <359005442@qq.com>
2020-12-02 09:59:11 +0800

    123

---
+++ 05/03/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//Mover is a type
+type Mover interface {
+	move()
+}
+
+type dog struct{}
+
+func (d *dog) move() {
+	fmt.Println("狗会动")
+}
+func main() {
+	var x Mover        // x可以接收dog类型
+	var fugui = &dog{} // 富贵是*dog类型
+	x = fugui          // x可以接收*dog类型
+	x.move()
+}
